84526f65e513e30b324a3654beefcd1a5390ff1a
tturdumamatovv <muki2022@mail.ru>
2024-09-27 15:39:48 +0600

    a

---
+++ apps/service/transtation.py
@@ -0,0 +1,7 @@
+from modeltranslation.translator import register, TranslationOptions
+from .models import ContactInfo
+
+
+@register(ContactInfo)
+class ContactInfoTranslationOptions(TranslationOptions):
+    fields = ('title',)  # Fields to translate
