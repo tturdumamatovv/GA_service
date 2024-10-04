ad0aab2a26d594a0da5878eb837f5b8e8f2ef9f8
Nurmuhammed <muki2022@mail.ru>
2024-10-04 16:24:29 +0600

    a

--- apps/service/views.py
+++ apps/service/views.py
@@ -82,10 +82,11 @@ def about(request):
     faq_section = FAQSection.objects.first()
     happy_customers_section = HappyCustomersSection.objects.first()
     blog_section = BlogSection.objects.first()
-    footer = FooterSection.objects.first()
+    footer = FooterSection.objects.prefetch_related('work_hours').first()
     about_section = AboutSection.objects.first()
     team_section = TeamSection.objects.first()
     google_section = GoogleSection.objects.first()
+    footer_work_hours = sorted(footer.work_hours.all(), key=lambda x: DAYS_ORDER.get(x.day, 0))
     return render(request, 'about-us.html', {
         'contact_info': contact_info,
         'navigation': navigation,
@@ -99,6 +100,7 @@ def about(request):
         'happy_customers_section': happy_customers_section,
         'blog_section': blog_section,
         'footer': footer,
+        'footer_work_hours': footer_work_hours,
         'about_section': about_section,
         'team_section': team_section,
         'google_section': google_section,
@@ -117,7 +119,8 @@ def service(request):
     faq_section = FAQSection.objects.first()
     happy_customers_section = HappyCustomersSection.objects.first()
     blog_section = BlogSection.objects.first()
-    footer = FooterSection.objects.first()
+    footer = FooterSection.objects.prefetch_related('work_hours').first()
+    footer_work_hours = sorted(footer.work_hours.all(), key=lambda x: DAYS_ORDER.get(x.day, 0))
     return render(request, 'services.html', {
         'contact_info': contact_info,
         'navigation': navigation,
@@ -131,6 +134,7 @@ def service(request):
         'happy_customers_section': happy_customers_section,
         'blog_section': blog_section,
         'footer': footer,
+        'footer_work_hours': footer_work_hours,
     })
 
 
@@ -146,7 +150,8 @@ def contact(request):
     faq_section = FAQSection.objects.first()
     happy_customers_section = HappyCustomersSection.objects.first()
     blog_section = BlogSection.objects.first()
-    footer = FooterSection.objects.first()
+    footer = FooterSection.objects.prefetch_related('work_hours').first()
+    footer_work_hours = sorted(footer.work_hours.all(), key=lambda x: DAYS_ORDER.get(x.day, 0))
     return render(request, 'contacts.html', {
         'contact_info': contact_info,
         'navigation': navigation,
@@ -160,4 +165,5 @@ def contact(request):
         'happy_customers_section': happy_customers_section,
         'blog_section': blog_section,
         'footer': footer,
+        'footer_work_hours': footer_work_hours,
     })
